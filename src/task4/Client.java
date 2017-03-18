bf65752751874db53ea1b17ce5860939d6fccf9a
bitscuit <li_zhao_bin@hotmail.com>
2017-03-17 23:05:46 -0400

    Client now keeps asking for input until exit or server shuts down. Refactored Client code. Only HELLO and ACK left to implement for both Client and Server. Need to check on server side whether file request is valid

--- src/task4/Client.java
+++ src/task4/Client.java
@@ -2,88 +2,165 @@ package task4;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
+
+	private static InetAddress serverAddr;
+	private static int port;
+	private static Socket clientSkt = null;
+
+	private static Scanner userInput = new Scanner(System.in);
+	private static File dir = new File("ClientFiles");
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		try{
-			System.out.println("Enter IP address");
-			Scanner input = new Scanner(System.in);
-			String s = input.nextLine();
-			//Get the IP address of the server
-			InetAddress serverAddr = InetAddress.getByName(s);
+		setupDir();
+		if (setupConnection()) {
+			getFileList();
+			sendRequests();
+			exit();
+		}
+	}
 
-			System.out.println("Enter port number");
-			s = input.nextLine();
-			//Server Port Number to Send Connection Request to
-			int serverTCPport = Integer.parseInt(s); //Receive Port Number from CmdLine
+	private static void setupDir() {
+		// make new directory for server files if it does not exist
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+	}
 
-			//Open the socket
-			//Socket to connect to the server at the IP address and port
-			Socket clientSkt = new Socket(serverAddr, serverTCPport);
+	private static boolean setupConnection() {
+		System.out.println("Enter IP address");
+		String s = userInput.nextLine();
 
-			byte[] b = new byte[100];
-			InputStream in1 = clientSkt.getInputStream();
-			BufferedInputStream buffIn1 = null;
-			buffIn1 = new BufferedInputStream(in1);
-			buffIn1.read(b, 0, b.length);
-			String s1 = new String(b);
-			System.out.println(s1);
+		// server IP address
+		try {
+			serverAddr = InetAddress.getByName(s);
+		} catch (UnknownHostException e) {
+			System.err.println("Could not establish host");
+		}
+		// server port 
+		System.out.println("Enter port number");
+		s = userInput.nextLine();
+		port = Integer.parseInt(s);
 
-			// request file
+		System.out.println("Setting up connection with server...");
+		//Open the socket
+		//Socket to connect to the server at the IP address and port
+		try {
+			clientSkt = new Socket(serverAddr, port);
+			// TODO: send hello frame
+			// TODO: accept ack frame
+			System.out.println("Connection established");
+		} catch (IOException e) {
+			System.err.println("Failed to connect to server");
+			return false;		
+		}
+		return true;
+	}
 
-			while (true) {
-				System.out.println("Enter file name to download");
-				s = input.nextLine();
-				if (s.toLowerCase().contains("exit")) {
+	private static void getFileList() {
+		System.out.println("Getting list of available files from server...");
+		String fileList = getInput();
+		System.out.println("List of Files: ");
+		System.out.println(fileList);
+	}
+
+	private static void sendRequests() {
+		System.out.println("Download file with \"download <file_name>\" or exit with \"exit\"");
+		String command = userInput.nextLine();
+		String[] args = command.split("\\s+");
+		String action = args[0].toLowerCase().trim();
+
+		while (true) {
+			if (action.equals("exit")) {
+				break;
+			} else if (action.equals("download")) {
+				sendOutput(command.getBytes());
+				String fileContent = getInput();
+				if (fileContent.equals("-1")) {
 					break;
 				}
-				byte[] filename = s.getBytes();
-				OutputStream outF = clientSkt.getOutputStream();
-				outF.write(filename, 0, filename.length);
-				outF.flush();
+				String filename = args[1];
+				writeFile(fileContent, filename);
 
-				//Client Waits for an incoming message from the server
-				//Client is blocked until Server replies
-				//			BufferedReader br = new BufferedReader(new InputStreamReader(clientSkt.getInputStream()));
-				//			System.out.println(br.readLine());
-				byte[] data = new byte[53];
-				InputStream in = clientSkt.getInputStream();
-				BufferedInputStream buffIn = null;
-				buffIn = new BufferedInputStream(in);
-
-				buffIn.read(data, 0, data.length);
-
-				File dir = new File("ClientFiles");
-				dir.mkdirs();
-				BufferedOutputStream out = null;
-				try {
-					out = new BufferedOutputStream(new FileOutputStream("ClientFiles" + File.separator + s));
-					out.write(data);
-					out.flush();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				System.out.println("Input command");
+				command = userInput.nextLine();
+				args = command.split("\\s+");
+				action = args[0].toLowerCase();
+			} else {
+				System.out.println("Invalid command");
 			}
-			//Connection release
-			clientSkt.close();
-		}catch(Exception e){
-			e.printStackTrace();
 		}
+	}
+
+	private static void writeFile(String s, String name) {
+		try (BufferedWriter out = 
+				new BufferedWriter(new FileWriter("ClientFiles" + File.separator + name))) {
+			out.write(s);
+			out.flush();
+		} catch (IOException e) {
+			System.err.println("Failed to write to file");
+		}
+	}
+
+	private static void exit() {
+		try {
+			clientSkt.close();
+		} catch (IOException e) {
+			System.err.println("Failed to close socket");
+		}
+	}
+
+	private static String getInput() {
+		InputStream in = null;
+		BufferedInputStream buffIn = null;
+		try {
+			in = clientSkt.getInputStream();
+			buffIn = new BufferedInputStream(in);
+		} catch (IOException e) {
+			System.err.println("Failed to get input stream");
+		}
+		byte[] b = new byte[1024];
+		System.out.println("Waiting for server response...");
+		try {
+			buffIn.read(b, 0, b.length);
+			System.out.println("Server response accepted");
+		} catch (IOException e) {
+			System.err.println("Could not read server response. Terminating session.");
+			return "-1";
+		}
+		String s = new String(b);
+		s = s.trim();
+		return s;
+	}
+
+	private static boolean sendOutput(byte[] b) {
+		OutputStream out = null;
+		BufferedOutputStream buffOut= null;
+		try {
+			out = clientSkt.getOutputStream();
+			buffOut = new BufferedOutputStream(out);
+		} catch (IOException e) {
+			System.err.print("Could not create output stream");
+		}
+
+		try {
+			buffOut.write(b, 0, b.length);
+			buffOut.flush();
+			return true;
+		} catch (IOException e) {
+			System.err.println("Could not send contents to output stream");
+		}
+		return false;
 	}
 }
