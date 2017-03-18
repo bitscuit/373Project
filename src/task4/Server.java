9e9e1804dd0ef4b443868226cc24a503937f3db0
bitscuit <li_zhao_bin@hotmail.com>
2017-03-17 20:05:27 -0400

    Server accepts a client command (download or exit) to download files to shutdown server. Refactored server code to be more modular. Server almost complete. Need the accept Hello from client first, then send ACK back, and receive ACK from client after every file download.

--- src/task4/Server.java
+++ src/task4/Server.java
@@ -1,15 +1,15 @@
 package task4;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -20,110 +20,41 @@ public class Server {
 
 	private static File file1 = new File("ServerFiles", "File1.txt");
 	private static File dir = new File("ServerFiles");
-	
+
 	private static int portNum = 10000;
 
 	public static void main(String[] args) {
-
 		setupFiles();
 		setupConnection();
 		listFiles();
-		String filename = getFileName();
-		byte[] file = readFile(filename);
-		sendFile(file);
-
-	}
-	
-	private static void listFiles() {
-		System.out.println("Sending client list of available files...");
-		OutputStream out = null;
-		try {
-			out = dataSkt.getOutputStream();
-		} catch (IOException e) {
-			System.err.print("Could not get socket output stream in listFiles()");
-		}
-		
-		String list = "";
-		for (File f : dir.listFiles()) {
-			if (f.isFile()) {
-				list += f.getName() + " ";
-			}
-		}
-		
-		byte[] b = list.getBytes();
-		try {
-			out.write(b, 0, b.length);
-		} catch (IOException e) {
-			System.err.println("Could not write filename in listFiles()");
-		}
-		
-		try {
-			out.flush();
-			System.out.println("File list sent");
-		} catch (IOException e) {
-			System.err.println("Could not flush output stream in listFiles()");
-		}
-	}
-
-	// Functional
-	private static String getFileName() {
-		System.out.println("Waiting for client request...");
-		InputStream in = null;
-		BufferedInputStream buffIn = null;
-		try {
-			in = dataSkt.getInputStream();
-			buffIn = new BufferedInputStream(in);
-		} catch (IOException e) {
-			System.err.println("Inputstream failed for filename");
-		}
-		byte[] b = new byte[10];
-		try {
-			buffIn.read(b, 0, b.length);
-			System.out.println("Request accepted");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.err.println("Could not read file");
-		}
-		String s = new String(b);
-		s = s.trim();
-		return s;
+		getClientRequest();
 	}
 
 	/**
-	 * Method to generate server files if they do not exist yet
+	 * Generates server files if they do not exist yet
 	 */
 	private static void setupFiles() {
+		// make new directory for server files if it does not exist
 		if (!dir.exists()) {
 			dir.mkdirs();
 		}
-		BufferedWriter out = null;
-		String s1 = "This is for file1.";
-
+		String s = "Dummy data for file1.";
+		// put dummy data in file
 		if (!(file1.exists())) {
 			System.out.println("File 1 doesn't exist");
-			try {
-				out = new BufferedWriter(new FileWriter("ServerFiles" + File.separator + file1.getName()));
-				out.write(s1);
+			try (BufferedWriter out = 
+					new BufferedWriter(new FileWriter("ServerFiles" + File.separator + file1.getName()))) {
+				out.write(s);
 				out.flush();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				System.err.println("Failed to write to file");
 			}
 		}
-
-		try {
-			if (out != null) {
-				out.close();
-
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
 	}
 
-	// Functional
+	/**
+	 * Sets up server and wait for client to connect
+	 */
 	private static void setupConnection() {
 		//Define Server Port Number to listen on
 		int servPort = portNum;
@@ -140,64 +71,165 @@ public class Server {
 		//Socket for accepting message
 		try {
 			dataSkt = conn.accept();
+//			String hello = getInput();
+			// sends ack frame to client
+			String ack = "ACK";
+//			sendOutput(ack.getBytes());
 			System.out.println("Established Connection");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			System.err.println("No connection");
 		}
 	}
 
-	private static byte[] readFile(String s) {
-		System.out.println("Copying contents of file...");
-		byte[] data = new byte[8192];
+	/**
+	 * List files that are in the ServerFiles directory, i.e. files available for download
+	 */
+	private static void listFiles() {
+		System.out.println("Sending client list of available files...");
+		// get list of files
+		String list = "";
+		for (File f : dir.listFiles()) {
+			if (f.isFile()) {
+				list += f.getName() + " ";
+			}
+		}
+		// send list
+		byte[] fileList = list.getBytes();
+		if (sendOutput(fileList)) {
+			System.out.println("File list sent to client");
+		} else {
+			System.out.println("Failed to send file list to client");
+		}
+	}
+
+	/**
+	 * Gets and parses client output to determine action. Either download files or exit
+	 */
+	private static void getClientRequest() {
+		String request = getInput();
+		String[] args = request.split("\\s+");
+		while (!args[0].toLowerCase().contains("exit")) {
+			if (args[0].toLowerCase().contains("download")) {
+				String filename = args[1].trim();
+
+				System.out.println("Copying contents of " + filename);
+				String file = readFile(filename);
+
+				System.out.println("Sending " + filename + " to client...");
+				sendFile(file);
+				
+//				String ack = getInput();
+//				System.out.println(ack);
+				// next request
+				request = getInput();
+				args = request.split("\\s+");
+			} else {
+				break;
+			}
+		}
+		System.out.println("Exiting");
+		exit();
+	}
+
+	/**
+	 * Retrieves whatever is in input stream for the server, i.e. get client output
+	 * @return String representation of input stream
+	 */
+	private static String getInput() {
 		InputStream in = null;
 		BufferedInputStream buffIn = null;
 		try {
-			in = new FileInputStream("ServerFiles" + File.separator + s);
+			in = dataSkt.getInputStream();
 			buffIn = new BufferedInputStream(in);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		try {
-			buffIn.read(data, 0, data.length);
-			System.out.println("File contents copied");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.println("Failed to get input stream");
 		}
-
-		return data;
-
+		byte[] b = new byte[1024];
+		System.out.println("Waiting for client request...");
+		try {
+			buffIn.read(b, 0, b.length);
+			System.out.println("Client request accepted");
+		} catch (IOException e) {
+			System.err.println("Could not read client request");
+		}
+		String s = new String(b);
+		s = s.trim();
+		return s;
 	}
 
-	private static void sendFile(byte[] b) {
-		System.out.println("Sending file to client...");
-		//Output stream of data to be sent to client
-		PrintStream sktOutput = null;
-		try {
-			sktOutput = new PrintStream(dataSkt.getOutputStream());
-		} catch (IOException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
+	/**
+	 * Reads file specified by the file name, s, and copies contents into a string
+	 * @param s The file name
+	 * @return String representation of the file contents
+	 */
+	private static String readFile(String s) {
+		String contents = "";
+		try (BufferedReader reader =
+				new BufferedReader(new FileReader("ServerFiles" + File.separator + s))) {
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				contents += line + System.lineSeparator();
+			}
+			System.out.println("File contents copied");
+		} catch (IOException e) {
+			System.err.println("Failed to copy file contents");
 		}
-		sktOutput.write(b, 0, b.length);
-		sktOutput.flush();
-		System.out.println("File sent");
+		return contents;
+	}
+	
+	/**
+	 * Sends the requested file to the client
+	 * @param s String representation of file contents
+	 */
+	private static void sendFile(String s) {
+		byte[] fileContent = s.getBytes();
+		if (sendOutput(fileContent)) {
+			System.out.println("File sent");
+		} else {
+			System.out.println("Failed to send file");
+		}
+	}
+
+	/**
+	 * Sends message to the server output stream, i.e, send to client input stream
+	 * @param b Byte array representation of message
+	 * @return True if and only if message was successfully put on server output stream
+	 */
+	private static boolean sendOutput(byte[] b) {
+		OutputStream out = null;
+		BufferedOutputStream buffOut= null;
+		try {
+			out = dataSkt.getOutputStream();
+			buffOut = new BufferedOutputStream(out);
+		} catch (IOException e) {
+			System.err.print("Could not create output stream");
+		}
+
+		try {
+			buffOut.write(b, 0, b.length);
+			buffOut.flush();
+			return true;
+		} catch (IOException e) {
+			System.err.println("Could not send contents to output stream");
+		}
+		return false;
+	}
+
+	/**
+	 * Shuts down the server by closing the socket and server socket
+	 */
+	private static void exit() {
 		//Close the Socket
 		try {
 			dataSkt.close();
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			System.err.println("Failed to close socket");
 		}
-		//Close or release connection
+		//Close connection
 		try {
 			conn.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.println("Failed to close connection");
 		}
 	}
 }
